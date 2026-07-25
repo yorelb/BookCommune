3da5298639901a3178c24560e20934a498d795e1
leroybarnie <lbarnie1@sheffield.ac.uk>
2026-07-25 14:56:41 +0100

    Borrowing and approving books in service

---
+++ book-commune/src/main/java/com/yorelb/book_commune/service/BorrowService.java
@@ -0,0 +1,69 @@
+package com.yorelb.book_commune.service;
+import com.yorelb.book_commune.model.Book;
+import com.yorelb.book_commune.model.BorrowRecord;
+import com.yorelb.book_commune.model.BorrowStatus;
+import com.yorelb.book_commune.model.User;
+import com.yorelb.book_commune.repository.BookRepository;
+import com.yorelb.book_commune.repository.BorrowRecordRepository;
+import com.yorelb.book_commune.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+public class BorrowService {
+
+    @Autowired
+    private BorrowRecordRepository borrowRepository;
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Borrowing a book. This is assuming user has clicked on the book
+    public BorrowRecord borrowBook(Long bookId, Long borrowerId) {
+
+        // Look for the book and user from the database
+        Optional<Book> bookOpt = bookRepository.findById(bookId);
+        Optional<User> userOpt = userRepository.findById(borrowerId);
+
+        if (bookOpt.isEmpty() || userOpt.isEmpty()) {
+            throw new IllegalArgumentException("Book or User not found!");
+        }
+
+        Book book = bookOpt.get();
+        User borrower = userOpt.get();
+
+        // Cant borrow own book
+        if (book.getOwner().getId().equals(borrowerId)) {
+            throw new IllegalArgumentException("You cannot borrow your own book.");
+        }
+
+        // Make the transaction
+        BorrowRecord record = new BorrowRecord();
+        record.setBook(book);
+        record.setBorrower(borrower);
+        record.setBorrowDate(LocalDate.now());
+        record.setDueDate(LocalDate.now().plusDays(14)); //TODO: Set a constant for borrow periods
+        record.setStatus(BorrowStatus.PENDING); //For pending, it switches to active once owner approves
+
+        return borrowRepository.save(record);
+    }
+
+    //Approving a borrow request. Assuming user has clicked approve
+    public BorrowRecord approveBorrowRequest(Long recordId) {
+        Optional<BorrowRecord> recordOpt = borrowRepository.findById(recordId);
+
+        if (recordOpt.isEmpty()) {
+            throw new IllegalArgumentException("Transaction not found.");
+        }
+
+        BorrowRecord record = recordOpt.get();
+        record.setStatus(BorrowStatus.ACTIVE); //Active one a user accepts to lend
+
+        return borrowRepository.save(record);
+    }
+}
