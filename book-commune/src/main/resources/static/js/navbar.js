001514c8b45ced7133e3c9dfff4c0b62d5cea525
leroybarnie <lbarnie1@sheffield.ac.uk>
2026-08-23 20:41:06 +0100

    Logout and minor colour change for edit fields

--- book-commune/src/main/resources/static/js/navbar.js
+++ book-commune/src/main/resources/static/js/navbar.js
@@ -19,3 +19,39 @@ fetch('/views/components/navbar.html')
         });
     })
     .catch(error => console.error('Error loading navbar:', error));
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarPlaceholder = document.getElementById('navbar-placeholder');
+
+    if (navbarPlaceholder) {
+        fetch('/views/components/navbar.html')
+            .then(response => {
+                if (!response.ok) throw new Error("Navbar not found!");
+                return response.text();
+            })
+            .then(html => {
+                navbarPlaceholder.innerHTML = html;
+
+                const currentPath = window.location.pathname;
+                const navLinks = document.querySelectorAll('.nav-link');
+
+                navLinks.forEach(link => {
+                    const activeKeyword = link.getAttribute('data-active');
+                    if (activeKeyword && currentPath.toLowerCase().includes(activeKeyword.toLowerCase())) {
+                        link.classList.add('active-box');
+                    }
+                });
+
+                const logoutBtn = document.getElementById('logoutBtn');
+
+                if (logoutBtn) {
+                    logoutBtn.addEventListener('click', (e) => {
+                        e.preventDefault();
+                        localStorage.removeItem('loggedInUser');
+                        window.location.href = '/views/startSession/login.html';
+                    });
+                }
+            })
+            .catch(error => console.error('Error loading navbar:', error));
+    }
+});
